4869301d9a7a099d6ad149eaa39a2cc590d8fdb2
Brett Slaski <brettski@yahoo.com>
2019-11-29 13:30:30 -0600

    updated for zsh

--- .zshrc
+++ .zshrc
@@ -1,1 +1,28 @@
+#export PATH="/usr/local/opt/node@10/bin:$PATH"
 
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/brettski/Downloads/google-cloud-sdk/path.zsh.inc' ]; then . '/Users/brettski/Downloads/google-cloud-sdk/path.zsh.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/Users/brettski/Downloads/google-cloud-sdk/completion.zsh.inc' ]; then . '/Users/brettski/Downloads/google-cloud-sdk/completion.zsh.inc'; fi
+
+if [[ -d "$HOME/.dotfiles" ]]; then
+	DOTFILES_DIR="$HOME/.dotfiles";
+	echo "dotfile directory: $DOTFILES_DIR"
+    # Load the shell dotfiles, and then some:
+    # * ~/.path can be used to extend `$PATH`.
+    for file in $DOTFILES_DIR/.{path,exports,alias}; do
+        [ -r "$file" ] && [ -f "$file" ] && source "$file";
+    done;
+    unset file;
+else
+	echo "unable to find .dotfiles folder, skipping"
+fi
+
+
+# zshell options/settings/updates
+setopt EXTENDED_HISTORY
+HISTSIZE=5000
+SAVEHIST=3000
+setopt APPEND_HISTORY
+setopt INC_APPEND_HISTORY
--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 set -e
 # Make sure we're up to date
-git pull origin master;
+#git pull origin master;
 
 # Get current dir
 
@@ -10,11 +10,20 @@ export DOTFILES_DIR DOTFILES_CACHE DOTFILES_EXTRA_DIR
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 function linkHome() {
-    ln -sfv "$DOTFILES_DIR/.bash_profile" ~
+    read -p "Link .base_profile? (y/n) : " -n 1;
+    echo ""
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        ln -sfv "$DOTFILES_DIR/.bash_profile" ~
+    fi;
+    read -p "Link .zshrc? (y/n) : " -n 1;
+    echo ""
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        ln -sfv "$DOTFILES_DIR/.zshrc" ~
+    fi;
+
     ln -sfv "$DOTFILES_DIR/.gitconfig" ~
     ln -sfv "$DOTFILES_DIR/.gitignore" ~
     ln -sfv "$DOTFILES_DIR/.vimrc" ~
-    ln -sfv "$DOTFILES_DIR/.zshrc" ~
     ln -sfv "$DOTFILES_DIR/.alias" ~
     ln -sfv "$DOTFILES_DIR/.exports" ~
     ln -sfv "$DOTFILES_DIR/.path" ~
@@ -23,6 +32,7 @@ function linkHome() {
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
     linkHome;
 else
+    echo "DOTFIRES_DIR: $DOTFILES_DIR"
     read -p "This will probably overwrite files in your home directory. Are you SURE you want to do this (y/n) : " -n 1;
     echo "";
     if [[ $REPLY =~ ^[Yy]$ ]]; then
@@ -34,3 +44,4 @@ unset linkHome;
 . $DOTFILES_DIR/.macos
 
 source ~/.bash_profile;
+source ~/.zshrc
