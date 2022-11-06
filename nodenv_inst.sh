2608a5e6e370648c1e5d37823421a657bb37a278
Brett Slaski <brettski@yahoo.com>
2022-11-05 21:30:07 -0500

    nodenv config. remove brew in ubuntu branch

---
+++ nodenv_inst.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+curl -fsSL https://raw.githubusercontent.com/nodenv/nodenv-installer/master/bin/nodenv-installer | bash
+
+# curl -fsSL https://raw.githubusercontent.com/nodenv/nodenv-installer/master/bin/nodenv-doctor | bash
+
