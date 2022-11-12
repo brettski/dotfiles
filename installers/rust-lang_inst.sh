466c52d3430aa80eb6e149073e4d7960924c63cc
Brett Slaski <brettski@yahoo.com>
2022-11-12 12:17:51 -0600

    refactor directories. Add installers

---
+++ installers/rust-lang_inst.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# References
+# https://www.rust-lang.org
+# https://www.rust-lang.org/tools/install
+# https://forge.rust-lang.org/infra/other-installation-methods.html
+
+curl https://sh.rustup.rs -sSf | sh
